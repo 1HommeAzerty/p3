345ee2516c8e84788fe5709f9a3b85af44802bb2
1HommeAzerty <carpentier.hugues@gmail.com>
2018-01-03 15:09:35 +0100

    Minor corrections

--- maze.py
+++ maze.py
@@ -1,46 +1,53 @@
 #! /usr/bin/env python3
 # coding: utf-8
-
+import random
 
 class Maze:
-  """docstring for maze"""
-  def __init__(self, file):
-    self.file = "level1.txt" #file
-    #self.structure = 0
-    # path, walls + guardian
+    """docstring for maze"""
+    def __init__(self, file):
+      self.file = file
+      #self.structure = 0
+      # path, walls + guardian
 
-  def generate(self):
-    with open(self.file, "r") as file:
-      level1 = []
-      free_positions = []
-           
-      for i, line in enumerate(file):      #parcours des lignes
-        line_lvl = []
-        
-        for j, sprite in enumerate(line): #parcours de chaque element          
-          if sprite != '\n':
-            line_lvl.append(sprite)
-          if sprite == " ":
-            free_positions.append((i, j))
+    def generate(self):
+      with open(self.file, "r") as f:
+        level1 = []
+        free_positions = []
             
-        level1.append(line_lvl)
+        for i, line in enumerate(f):      #parcours des lignes
+          line_lvl = []
+          
+          for j, sprite in enumerate(line): #parcours de chaque element          
+            if sprite != '\n':
+              line_lvl.append(sprite)
+            if sprite == " ":
+              free_positions.append((i, j))
+              
+          level1.append(line_lvl)
 
-      self.structure = level1
-      #self.path = free_positions
-      print(free_positions)
+        self.structure = level1
+        self.paths = free_positions
+        print(free_positions)
 
-  def position_randomly(self, display):
-    pass
-    
-  def getdisplay(self):
-    return maze1
+    def position_randomly(self):
+      
+      items = ["N", "E", "T"]
+      positions = random.sample(self.paths, 3)
 
-  def display(self):
-    for line in self.structure:
-      print("".join(line))
+      for position, item in zip(positions, items):
+          print(position, item)   
+
+      
+    def getdisplay(self):
+      return self.structure
+
+    def display(self):
+      for line in self.structure:
+        print("".join(line))
 
 def main():
-  maze1 = Maze("level1.txt")
-  maze1.generate()
-  maze1.display()
+    maze1 = Maze("level1.txt")
+    maze1.generate()
+    maze1.display()
+    maze1.position_randomly()
 main()
