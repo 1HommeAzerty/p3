1c3ee89cef57a7baeb0bedf819fd536409e16380
1HommeAzerty <carpentier.hugues@gmail.com>
2018-01-03 15:22:55 +0100

    Minor corrections, indent

--- maze.py
+++ maze.py
@@ -10,20 +10,20 @@ class Maze:
       # path, walls + guardian
 
     def generate(self):
-      with open(self.file, "r") as f:
-        level1 = []
-        free_positions = []
+        with open(self.file, "r") as f:
+            level1 = []
+            free_positions = []
             
-        for i, line in enumerate(f):      #parcours des lignes
-          line_lvl = []
+            for i, line in enumerate(f):      #parcours des lignes
+                line_lvl = []
           
-          for j, sprite in enumerate(line): #parcours de chaque element          
-            if sprite != '\n':
-              line_lvl.append(sprite)
-            if sprite == " ":
-              free_positions.append((i, j))
+                for j, sprite in enumerate(line): #parcours de chaque element          
+                    if sprite != '\n':
+                        line_lvl.append(sprite)
+                    if sprite == " ":
+                        free_positions.append((i, j))
               
-          level1.append(line_lvl)
+                level1.append(line_lvl)
 
         self.structure = level1
         self.paths = free_positions
@@ -31,19 +31,19 @@ class Maze:
 
     def position_randomly(self):
       
-      items = ["N", "E", "T"]
-      positions = random.sample(self.paths, 3)
+        items = ["N", "E", "T"]
+        positions = random.sample(self.paths, 3)
 
-      for position, item in zip(positions, items):
-          print(position, item)   
+        for position, item in zip(positions, items):
+            print(position, item)   
 
       
     def getdisplay(self):
-      return self.structure
+        return self.structure
 
     def display(self):
-      for line in self.structure:
-        print("".join(line))
+        for line in self.structure:
+            print("".join(line))
 
 def main():
     maze1 = Maze("level1.txt")
