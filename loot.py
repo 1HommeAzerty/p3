058f9d042f55b5e3d9c3afb25e947e1a8292da42
1HommeAzerty <carpentier.hugues@gmail.com>
2018-01-05 15:58:40 +0100

    Added first draft of missing classes

---
+++ loot.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+class Loot:
+    def __init__(self, structure):
+        pass
+
+def main():   
+    needle = Loot('N')
+    ether = Loot('E')
+    tube = Loot('T')
+    
