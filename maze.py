b077f9001e1d36c1a01c0acbd5f52c20ffa97d49
1HommeAzerty <carpentier.hugues@gmail.com>
2018-01-21 01:56:45 +0100

    Added items to the maze and a method to pick them up

--- maze.py
+++ maze.py
@@ -2,42 +2,74 @@
 # coding: utf-8
 import random
 
+
 class Maze:
     """docstring for maze"""
-    def __init__(self, file):
-      self.file = file
-      #self.structure = 0
-      # path, walls + guardian
+    def __init__(self, configfile):
+        self.file = configfile
+        #self.structure = 0
+        # path, walls + guardian
 
     def generate(self):
         with open(self.file, "r") as f:
             level1 = []
             free_positions = []
-            
-            for i, line in enumerate(f):      #parcours des lignes
+            wall_positions = []
+            mac_position = None
+            guardian_position = None
+
+            for i, line in enumerate(f):    #parcours des lignes
                 line_lvl = []
-          
-                for j, sprite in enumerate(line): #parcours de chaque element          
+                
+                for j, sprite in enumerate(line):   #parcours de chaque element
                     if sprite != '\n':
                         line_lvl.append(sprite)
                     if sprite == " ":
                         free_positions.append((i, j))
-              
+                    elif sprite == 'x':
+                        wall_positions.append((i,j))
+                    elif sprite == 'M':
+                        mac_position = i, j
+                    elif sprite == 'G':
+                        guardian_position = i, j
                 level1.append(line_lvl)
 
         self.structure = level1
         self.paths = free_positions
-        print(free_positions)
+        self.wall_positions = wall_positions
+        self.mac_position = mac_position
+        self.guardian_position = guardian_position
+        self.item_pos = []
 
-    def position_randomly(self):
-      
-        items = ["N", "E", "T"]
-        positions = random.sample(self.paths, 3)
-
+    def position_randomly(self, items):
+        k = len(items)
+        positions = random.sample(self.paths, k)
         for position, item in zip(positions, items):
-            print(position, item)   
+            (x, y) = position
+            self.structure[x][y] = item
+            self.item_pos.append(position)
+        
+    def get_item_pos(self):
+        return self.item_pos
 
-      
+    def get_mac_position(self):
+        return self.mac_position 
+
+    def get_guardian_position(self):
+        return self.guardian_position
+
+    def move_mac(self, new_position):
+        actual_position = self.get_mac_position()
+        (x, y) = actual_position
+        self.structure[x][y] = ' '
+        (x, y) = new_position
+        self.structure[x][y] = 'M'
+        self.mac_position = new_position
+
+
+    def is_wall (self, x, y):
+        return (x, y) in self.wall_positions 
+
     def getdisplay(self):
         return self.structure
 
@@ -48,6 +80,8 @@ class Maze:
 def main():
     maze1 = Maze("level1.txt")
     maze1.generate()
-    maze1.display()
     maze1.position_randomly()
-main()
+    maze1.display()
+    maze1.is_wall()
+if __name__ == "__main__":
+    main()
